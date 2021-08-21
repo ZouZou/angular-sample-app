d577e1e54bc7c917c997826762484e05905581bc
Joseph Jabbour <jabbour.joseph@gmail.com>
2021-08-21 17:58:13 +0300

    commit changes

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+interface UserInterface {
+  name: string;
+  age: string;
+  id: number;
+  isColored: boolean;
+}
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.sass']
+})
+export class UserComponent implements OnInit {
+
+  @Input() user: UserInterface;
+  @Output() userEvent = new EventEmitter<UserInterface>();
+  isColored: boolean;
+
+  constructor() { 
+    this.user = {} as UserInterface;
+    this.isColored = this.user.isColored ? true : false;
+  }
+
+  ngOnInit(): void {
+    this.isColored = this.user.isColored ? true : false;
+  }
+
+  sendUserEvent(): void {
+    this.userEvent.emit(this.user);
+  }
+}
